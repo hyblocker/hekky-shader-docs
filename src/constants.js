0d5f0ba6b7753ee7168e4be415fa6a06fd795487
Hyblocker <hyblocker@protonmail.com>
2022-06-28 18:21:14 +0200

    Localization support

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 export const SITE = {
-    title:      "Documentation",
-    domain:     "docs.hyblocker.dev",
-    embedColor: "#168CDD",
-    docsRoot:   "/"
+    title:          "Documentation",        // The name of the documentation, appears in <title>
+    domain:         "docs.hyblocker.dev",   // The domain these docs will be hosted on. Used for URL resolution
+    root:           "/en/",                 // A URL used to redirect to home. Only affects the Header
+    embedColor:     "#168CDD",              // A hex colour code, used for embeds on social platforms
+    docsRoot:       "/en/",                 // Relative path to the root page of the primary language docs are offered in
+    localizedHome:  false                   // Whether the home page link should link to a localized URL or not
 };
 
 export const SOCIALS = {
