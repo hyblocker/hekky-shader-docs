0272414f759117054d157210081d0a5dd73ed071
Hyblocker <hyblocker@protonmail.com>
2022-07-01 16:42:08 +0200

    Add edit on GitHub link to pages

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,11 @@ export const SITE = {
     root:           "/en/",                 // A URL used to redirect to home. Only affects the Header
     embedColor:     "#168CDD",              // A hex color code, used for embeds on social platforms
     docsRoot:       "/en/",                 // Relative path to the root page of the primary language docs are offered in
-    localizedHome:  false                   // Whether the home page link should link to a localized URL or not
+    localizedHome:  false,                  // Whether the home page link should link to a localized URL or not
+    // Edit on GitHub props
+    // To make this work on GitLab: https://gitlab.com/someuser/somerepo/-
+    gitRepo:        "https://github.com/hyblocker/hekky-shader-docs",
+    gitBranch:      "master"
 };
 
 export const SOCIALS = {
